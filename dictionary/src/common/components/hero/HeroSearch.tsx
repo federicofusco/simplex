a0b5ab546cceafb560977c9e0bff7b03ef365d92
federicofusco <40967674+federicofusco@users.noreply.github.com>
2023-02-25 23:23:23 +0100

    Added keyboard shortcuts, removed some buttons which currently can't work

--- dictionary/src/common/components/hero/HeroSearch.tsx
+++ dictionary/src/common/components/hero/HeroSearch.tsx
@@ -1,9 +1,30 @@
+import { useRef, useCallback, useEffect } from "react";
+
 const HeroSearch = () => {
+    const SEARCH_SHORTCUT = 's';
+    const searchBarRef = useRef<HTMLInputElement> ( null );
+    const focusSearchBar = () => {
+        searchBarRef.current?.focus ();
+        console.log(1);
+    }
+
+    const handleKeyPress = useCallback((event: any) => {
+        if (event.key === SEARCH_SHORTCUT) focusSearchBar ();
+    }, []);
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress);
+
+        return () => { document.removeEventListener('keydown', handleKeyPress) };
+    }, [handleKeyPress]);
+
+
     return <div className="border border-gray-300 rounded-sm flex justify-center mt-8 w-full max-w-md">
         <input 
-        placeholder="Click or press 'S' to search" 
-        type="text" 
-        className="w-full p-2 outline-none"
+            ref={ searchBarRef }
+            placeholder="Click or press 'S' to search" 
+            type="text" 
+            className="w-full p-2 outline-none"
         />
         <button className="p-2 text-gray-400 border-l border-gray-300 hover:bg-gray-100">Search</button>
     </div>
